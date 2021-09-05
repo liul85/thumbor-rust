39ea94056bd50216dea035021ca5619ac2642be5
liul85 <liul85@gmail.com>
2021-09-05 20:59:30 +1000

    handle http request and process parameters

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,45 @@
+use std::convert::TryInto;
+
+use axum::extract::Path;
+use axum::handler::get;
+use axum::http::StatusCode;
+use axum::Router;
+use percent_encoding::percent_decode_str;
+use serde::Deserialize;
+
+use pb::*;
+
 mod pb;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Deserialize)]
+struct Params {
+    spec: String,
+    url: String,
+}
+
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/image/:spec/:url", get(generate));
+
+    let addr = "127.0.0.1:3000".parse().unwrap();
+
+    tracing::debug!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await.unwrap();
+}
+
+async fn generate(Path(Params { spec, url }): Path<Params>) -> Result<String, StatusCode> {
+    let url = percent_decode_str(&url).decode_utf8_lossy();
+    let spec: ImageSpec = spec
+        .as_str()
+        .try_into()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    Ok(format!("url {}\n spec: {:#?}", url, spec))
 }
